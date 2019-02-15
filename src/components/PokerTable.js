49942c0d571c852edf6a080807d0fc80a0d222a9
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-15 15:55:30 -0500

    added images

--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Deck from '../utilityClasses/Deck';
 import GameButtons from './GameButtons';
+import PokerHand from './PokerHand';
 
 class PokerTable extends Component{
     constructor(){
@@ -10,16 +11,35 @@ class PokerTable extends Component{
         this.cards.shuffleDeck();
         console.log(this.cards);
         this.state = {
-
+            playersHand: ['blue_back','blue_back'],
+            dealersHand: ['blue_back','blue_back']
         }
+        this.prepDeck = this.prepDeck.bind(this);
     }
 
+    // This is a custom METHODS. Not coming from "React"
+    // We can't put this in our Deck class, because it's specific to our Hold em
+    prepDeck(){
+        this.cards.deck.shift(); // burn card
+        let dealCard = this.cards.deck.shift(); 
+        const card1 = dealCard;
+        const card2 = dealCard;
+        const card3 = dealCard;
+        const card4 = dealCard;
+        // deck is now only 48, becuase we mutated this.deck when we ran shift 
+        this.setState({
+            playersHand: [card1, card3],
+            dealersHand: [card2, card4],
+        })
+    }
 
 
     render(){
         return(
             <div className="col-sm-12 the-table">
-                <GameButtons />
+                <PokerHand cards={this.state.dealersHand} />
+                <PokerHand cards={this.state.playersHand} />
+                <GameButtons dealFunction={this.prepDeck}/>
             </div>
         )
     }
